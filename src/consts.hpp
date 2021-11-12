74c1c4fe0d75d88cf0f3a6037599c2b4a089f7a9
Jarpos <jar.pos@protonmail.com>
2021-11-12 11:04:20 +0100

    add consts file with standard alphabet

---
+++ src/consts.hpp
@@ -0,0 +1,6 @@
+#if !defined(__CONSTS_42_HPP__)
+#define __CONSTS_42_HPP__
+
+inline constexpr char ALPHABET[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+#endif // __CONSTS_42_HPP__
