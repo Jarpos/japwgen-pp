1d69a6da8c5f7901e53662c9acd96db8dc355138
Jarpos <jar.pos@protonmail.com>
2021-11-12 11:05:29 +0100

    feat: add basic setup for pw generation and output

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
+#include "consts.hpp"
 
-int main(int argc, char const *argv[])
+#include <iostream>
+#include <list>
+#include <string>
+
+std::list<std::string> GeneratePasswords(std::string charset, size_t pwlength, int pwcount);
+std::string GeneratePassword(std::string charset, size_t pwlength);
+
+int main(int argc, char const* argv[])
 {
-	return 0;
+	int i = 1;
+    std::list<std::string> pws = GeneratePasswords(ALPHABET, 10, 10 * 10);
+    for (auto& pw : pws) {
+        std::cout << pw << " ";
+		if (i++ % 5 == 0) {
+			std::cout << "\n";
+		}
+    }
+    return 0;
 }
