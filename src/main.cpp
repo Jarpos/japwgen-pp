0208ab514e0322f93f0ca6fe22f75055c24bd1c5
Jarpos <jar.pos@protonmail.com>
2021-11-12 11:25:51 +0100

    style: rename & formatting

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,18 @@
 #include <list>
 #include <string>
 
-std::list<std::string> GeneratePasswords(std::string charset, size_t pwlength, int pwcount);
+std::list<std::string> GeneratePasswords(std::string charset, size_t len, int count);
 std::string GeneratePassword(std::string charset, size_t pwlength);
 
 int main(int argc, char const* argv[])
 {
-	int i = 1;
+    int i = 1;
     std::list<std::string> pws = GeneratePasswords(ALPHABET, 10, 10 * 10);
     for (auto& pw : pws) {
         std::cout << pw << " ";
-		if (i++ % 5 == 0) {
-			std::cout << "\n";
-		}
+        if (i++ % 5 == 0) {
+            std::cout << "\n";
+        }
     }
     return 0;
 }
