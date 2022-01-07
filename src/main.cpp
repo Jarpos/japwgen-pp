280746ef44f27fce24353b63219e85e3d61040b1
Jarpos <jar.pos@protonmail.com>
2022-01-07 21:19:08 +0100

    update help text with new options; add example to help text

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "consts.hpp"
 
+#include <filesystem>
 #include <iostream>
 #include <list>
 #include <string>
@@ -7,7 +8,7 @@
 std::list<std::string> GeneratePasswords(std::string charset, size_t len, int count);
 void ParseArgs(options& opts, int argc, const char* argv[]);
 
-void PrintHelp();
+void PrintHelp(const std::string& progname);
 
 int main(int argc, char const* argv[])
 {
@@ -15,7 +16,8 @@ int main(int argc, char const* argv[])
     if (argc > 1) {
         ParseArgs(opts, argc, argv);
         if (opts.help) {
-            PrintHelp();
+            auto p = std::filesystem::path(argv[0]);
+            PrintHelp(p.stem().string());
             return EXIT_SUCCESS;
         }
     }
@@ -33,17 +35,22 @@ int main(int argc, char const* argv[])
     return EXIT_SUCCESS;
 }
 
-void PrintHelp()
+void PrintHelp(const std::string& progname)
 {
-    std::printf("Usage: japwgen-pp [ OPTIONS ]\n");
+    const options o; // Use default values for example
+    std::printf("Usage: %s [ OPTIONS ]\n", progname.c_str());
+    std::printf("Example: %s ", progname.c_str());
+    std::printf("-c %d -l %d ", o.pwcnt, o.pwlen);
+    std::printf("--alphabet %s\n", o.alphabet.c_str());
     std::printf("\n");
+
     std::printf("Options:\n");
-    std::printf("\t--help\n");
-    std::printf("\t    Displays this message\n");
-    std::printf("\t--alphabet [ ALPHABET ]\n");
-    std::printf("\t    Specify alternative alphabet to use for pw generation\n");
-    std::printf("\t--count [ NUMBER ]\n");
-    std::printf("\t    Specify amount of generated passwords\n");
-    std::printf("\t--length [ NUMBER ]\n");
-    std::printf("\t    Specify length of each generated password\n");
+    std::printf("\t-h|--help\n");
+    std::printf("\t   Displays this message\n");
+    std::printf("\t-a|--alphabet [ ALPHABET ]\n");
+    std::printf("\t   Specify alternative alphabet to use for pw generation\n");
+    std::printf("\t-c|--count [ NUMBER ]\n");
+    std::printf("\t   Specify amount of generated passwords\n");
+    std::printf("\t-l|--length [ NUMBER ]\n");
+    std::printf("\t   Specify length of each generated password\n");
 }
