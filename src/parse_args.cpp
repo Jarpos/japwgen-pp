441748c58d696017ebf29d97d9288b2e3cb54f54
Jarpos <jar.pos@protonmail.com>
2022-01-07 21:02:49 +0100

    add ways to cope with invalid argv so the program doesn't segfault

--- src/parse_args.cpp
+++ src/parse_args.cpp
@@ -1,20 +1,45 @@
 #include "consts.hpp"
 
 #include <string>
+#include <system_error>
+
+void SetOption(int& option, const char* argv[], int argc, int i);
+void SetOption(std::string& option, const char* argv[], int argc, int i);
 
 void ParseArgs(options& opts, int argc, const char* argv[])
 {
+    std::string arg;
     for (int i = 1; i < argc; i++) {
-        std::string arg = argv[i];
+        SetOption(arg, argv, argc, i);
 
-        if (arg == "--alphabet") {
-            opts.alphabet = argv[++i];
-        } else if (arg == "--count") {
-            opts.pwcnt = std::stoi(argv[++i]);
-        } else if (arg == "--length") {
-            opts.pwlen = std::stoi(argv[++i]);
-        } else if (arg == "--help") {
+        if (arg == "--alphabet" || arg == "-a") {
+            SetOption(opts.alphabet, argv, argc, ++i);
+        } else if (arg == "--count" || arg == "-c") {
+            SetOption(opts.pwcnt, argv, argc, ++i);
+        } else if (arg == "--length" || arg == "-l") {
+            SetOption(opts.pwlen, argv, argc, ++i);
+        } else if (arg == "--help" || arg == "-h") {
             opts.help = true;
         }
+    }
+}
+
+void SetOption(int& option, const char* argv[], int argc, int i)
+{
+    // Use standard value if index is out of bounds
+    if (i < argc) {
+        try {
+            option = std::stoi(argv[i]);
+        } catch (...) {
+            // Ignore Exception, and just use the standard value
+        }
+    }
+}
+
+void SetOption(std::string& option, const char* argv[], int argc, int i)
+{
+    // Use standard value if index is out of bounds
+    if (i < argc) {
+        option = argv[i];
     }
 }
